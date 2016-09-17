6addf8aa73e7b6e54f9bd4001e1ffa4506f0c729
Cooper Knaak <cooperknaak@gmail.com>
2016-09-16 19:46:51 -0500

    Initial Commit

---
+++ VoronoiVisualizer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VoronoiVisualizer
+//
+//  Created by Cooper Knaak on 9/16/16.
+//  Copyright © 2016 Cooper Knaak. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VoronoiVisualizer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VoronoiVisualizer
+//
+//  Created by Cooper Knaak on 9/16/16.
+//  Copyright © 2016 Cooper Knaak. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
